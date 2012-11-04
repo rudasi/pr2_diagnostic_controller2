1c294334d0377418669284408025064c11501cc5
Rahul Udasi <rahuludasi@hotmail.com>
2012-11-04 15:45:20 -0800

    go through

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -18,36 +18,60 @@ stream = file(fn, 'r')
 samples = load(stream)
 velocity = []
 encoder_position = []
-ppar = 1200.0
+ppr = 1200.0
 delay = 1 
 filt1 = numpy.array([-1,1])
-
-def cut(d, begin, end):
-  return d[begin:end]
 
 def get_acceleration(velocity):
   acceleration = numpy.convolve(filt1, velocity)
   acceleration = acceleration[delay:len(velocity)+delay]
   acceleration = abs(acceleration)
+  print velocity 
   acceleration = acceleration / acceleration.max()
   return acceleration
 
 def check_for_spikes(spikes):
-  sorted_data = numpy.sort(spikes,kind='mergesort')
-  iq_range = scoreatpercentile(sorted_data,75) - scoreatpercentile(sorted_data,25)
-  outlier_limit = iq_range * 3 + scoreatpercentile(sorted_data,75)
+#sorted_data = numpy.sort(spikes,kind='mergesort')
+# iq_range = scoreatpercentile(sorted_data,75) - scoreatpercentile(sorted_data,25)
+# outlier_limit = iq_range * 3 + scoreatpercentile(sorted_data,75)
+  neg = [val for val in spikes if val < 0]
+  pos = [val for val in spikes if val > 0]
 
-  if (outlier_limit == 0):
-    print "using mean multiplier as outlier limit"
-    outlier_limit = 5 * spikes.mean()
+  neg = numpy.sort(neg,kind='mergesort')
+  pos = numpy.sort(pos,kind='mergesort')
+  
+  if (len(neg) > 1):
+    iq_range_neg = scoreatpercentile(neg,-75) - scoreatpercentile(neg,-25)
+#print "iqr75 is ", scoreatpercentile(neg,-75), " iqr25 is ", scoreatpercentile(neg,-25)
+    outlier_limit_neg = iq_range_neg * 3 + scoreatpercentile(neg,-75)
 
-  print "mean",spikes.mean()
-  print "outlier limit",outlier_limit
+  if (len(pos) > 1):
+    iq_range_pos = scoreatpercentile(pos,75) - scoreatpercentile(pos,25)
+#  print "iqr75 is ", scoreatpercentile(pos,75), " iqr25 is ", scoreatpercentile(pos,25)
+    outlier_limit_pos = iq_range_pos * 3 + scoreatpercentile(pos,75)
 
-  outliers = [val for val in spikes if val > outlier_limit]
+#sorted_data = numpy.sort(spikes,kind='mergesort')
+# iq_range = scoreatpercentile(sorted_data,75) - scoreatpercentile(sorted_data,25)
+#  outlier_limit = iq_range * 3 + scoreatpercentile(sorted_data,75)
 
-  print "outliers in filtered data", outliers
-  if len(outliers) >= 2:
+#print "mean",spikes.mean()
+# print "outlier limit",outlier_limit
+  print "length of neg is ",len(neg)," len of pos is ",len(pos)
+ 
+  if (len(neg) > 1):
+    print "neg mean",neg.mean()
+    print "neg outlier limit",outlier_limit_neg
+  
+  if (len(pos) > 1):
+    print "pos mean",pos.mean()
+    print "pos outlier limit",outlier_limit_pos
+
+  outliers_neg = [val for val in neg if val < outlier_limit_neg]
+  outliers_pos = [val for val in pos if val > outlier_limit_pos]
+
+  print "outliers in filtered data", outliers_neg, outliers_pos
+#if len(outliers) >= 2:
+  if (len(outliers_neg) + len(outliers_pos)) >= 2:
     print "Encoder could be spoilt"
 
 def check_for_unplugged(velocity):
@@ -67,16 +91,19 @@ for s in samples.sample_buffer:
   velocity.append(s.velocity)
   encoder_position.append(s.encoder_position)
 
+#a = numpy.array(encoder_position)
+#encoder_velocity = a[1:] - a[:-1]
+#encoder_velocity = (encoder_velocity / (2*numpy.pi)) * ppr
 velocity = numpy.array(velocity)
 encoder_position = numpy.array(encoder_position)
-
+#encoder_position_mod = encoder_position % (2*numpy.pi)
 acceleration = get_acceleration(velocity)
-
-spikes = acceleration * velocity
-
+print encoder_position
+#spikes = acceleration * (1.0/(abs(velocity) + 1))
+spikes = acceleration * (velocity)
+#velocity_ticks = (velocity / (2*numpy.pi)) * ppr / 1000.0
 check_for_spikes(spikes)
 check_for_unplugged(velocity)
-
 
 plt.figure()
 plt.subplot(311)
@@ -84,12 +111,18 @@ plt.plot(velocity,label='velocity')
 plt.legend()
 
 plt.subplot(312)
-plt.plot(spikes,'*-', label='acceleration * velocity')
+plt.plot(spikes,label='acceleration * abs(velocity)')
+#plt.plot(encoder_position_mod[:-2],spikes[:-2],'*',label='acceleration * abs(velocity)')
 plt.legend()
 
 plt.subplot(313)
-plt.plot(acceleration,'g*-', label='acceleration')
+#plt.plot(encoder_position_mod[:-2],acceleration[:-2],'g*', label='acceleration')
+plt.plot(acceleration,'g', label='acceleration')
 plt.legend()
+
+#plt.subplot(414)
+#plt.plot(encoder_position_mod,'g*-', label='encoder_position')
+#plt.legend()
 
 if False:
   plt.figure()
